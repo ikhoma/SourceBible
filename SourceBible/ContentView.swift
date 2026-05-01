5a9dfa49c27ba3e488ff6bcabb9890836f24a437
Ivan Khoma <ivan.khoma@example.com>
2026-05-01 21:34:01 +0300

    Initial Commit

---
+++ SourceBible/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SourceBible
+//
+//  Created by ivan.khoma on 01.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SourceBible/SourceBibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceBibleApp.swift
+//  SourceBible
+//
+//  Created by ivan.khoma on 01.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct SourceBibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
